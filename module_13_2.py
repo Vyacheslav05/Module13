fe95c3638f1cbd5e4330f63649a65e64199b05ad
Vyacheslav05 <vyacheslav.lobanov@wellpack.org>
2024-10-03 13:07:25 +0300

    Update module_13_2.py

--- module_13_2.py
+++ module_13_2.py
@@ -8,7 +8,7 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 
 @dp.message_handler(commands=['Start'])
 async def start_message(message):
-    print('Привет! Я бот помогающий твоему здоровью.')
+    print('Привет! Я бот, помогающий твоему здоровью.')
 
 @dp.message_handler()
 async def all_message(message):
